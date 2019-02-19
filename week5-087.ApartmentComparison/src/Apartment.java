54d7ddc2fe1b5eb37d8109328b2e4553ced5718c
swifferwarrior <kdhailey21@gmail.com>
2019-02-19 13:28:57 -0600

    Completed Exercise 87.

--- week5-087.ApartmentComparison/src/Apartment.java
+++ week5-087.ApartmentComparison/src/Apartment.java
@@ -4,11 +4,46 @@ public class Apartment {
     private int rooms;
     private int squareMeters;
     private int pricePerSquareMeter;
+    private int totalPrice = squareMeters * pricePerSquareMeter;
 
     public Apartment(int rooms, int squareMeters, int pricePerSquareMeter) {
         this.rooms = rooms;
         this.squareMeters = squareMeters;
         this.pricePerSquareMeter = pricePerSquareMeter;
+        this.totalPrice = this.squareMeters * this.pricePerSquareMeter;
+    }
+
+    public boolean larger(Apartment otherApartment) {
+        /* Returns true if the Apartment object for which the method is
+        called (this) is larger than the apartment object given as
+        parameter (otherApartment).
+         */
+
+        if (this.squareMeters > otherApartment.squareMeters) {
+            return true;
+        }
+
+        return false;
     }
     
+    public int priceDifference (Apartment otherApartment) {
+        int priceDifference = 0;
+        
+        if (moreExpensiveThan (otherApartment)) {
+            priceDifference = this.totalPrice - otherApartment.totalPrice;
+        } else if (!moreExpensiveThan (otherApartment)) {
+            priceDifference = otherApartment.totalPrice - this.totalPrice;
+        }
+        
+        return priceDifference;
+    }
+    
+    public boolean moreExpensiveThan (Apartment otherApartment) {
+        if(this.totalPrice > otherApartment.totalPrice) {
+            return true;
+        }
+        
+        return false;
+    }
 }
+
