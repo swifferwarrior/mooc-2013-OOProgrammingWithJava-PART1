896fbfed4a06ff0b0cf340d641526b4c19957fc8
swifferwarrior <kdhailey21@gmail.com>
2019-04-27 23:11:52 -0500

    Completed Exc. 106. PART 1 DONE!

--- week6-106.BinarySearch/src/BinarySearch.java
+++ week6-106.BinarySearch/src/BinarySearch.java
@@ -1,16 +1,50 @@
+
 public class BinarySearch {
+
     public static boolean search(int[] array, int searchedValue) {
+        //finish the method into a real binary search
         int beginning = 0;
         int end = array.length - 1;
 
         while (beginning <= end) {
             int middle = (beginning + end) / 2;
+            //System.out.println(beginning + ", " + middle + ", " + end);       Prints out the variables for each loop.
             if (array[middle] == searchedValue) {
+                //System.out.println("Value " + searchedValue + " is in the array");
                 return true;
             }
 
             // restrict the search area 
+            if (middle == (array.length - 2)) {
+                if (array[array.length - 1] == searchedValue) {
+                    //System.out.println("Value " + searchedValue + " is in the array");
+                    return true;
+                }
+            }
+
+            if (searchedValue > array[middle]) {
+                beginning = middle + 1;
+            } else if (searchedValue < array[middle]) {
+                end = middle;
+            }
+
+            if (end == beginning && array[middle] != searchedValue) {
+                break;
+            }
         }
+
+        //System.out.println("Value " + searchedValue + " is not in the array");
         return false;
     }
 }
+
+/* POST-SUBMIT NOTES */
+ /*
+1. HOW TO DETECT LAST INDEX IN THE ARRAY
+        If the middle is found by averaging, then the answer is always rounded
+        down and it won't report the last index as in the array. This is fixed by
+        stating that if the middle equals the second to last index, check if the 
+        last index matches the searched value before anything else.
+2. DON'T PRINT IN THIS CLASS
+        Pring in the main class.
+ */
--- week6-106.BinarySearch/src/Main.java
+++ week6-106.BinarySearch/src/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         // Test your program here
         int[] taulukko = {-3, 2, 3, 4, 7, 8, 12};
+        
         Scanner lukija = new Scanner(System.in);
+        //Arrays.sort(taulukko);
 
         System.out.print("Numbers in the array " + Arrays.toString(taulukko));
         System.out.println();
@@ -19,5 +21,21 @@ public class Main {
         boolean tulos = BinarySearch.search(taulukko, Integer.parseInt(etsittavaLuku));
 
         // Print here the result
+        if (tulos) {
+            System.out.println("Value " + etsittavaLuku + " is in the array");
+        } else {
+            System.out.println("Value " + etsittavaLuku + " is not in the array");
+
+        }
     }
 }
+/* POST-SUBMIT NOTES */
+/*
+1. SORT THE ARRAY
+        The values in the array appear to be in order already, but for some reason
+        it will time out unless you sort it first...why? Reddit states that it's 
+        possibly as a precaution if a new, out-of-order entry was made into the 
+        array. But it doesn't quite explain why omitting a sort() method would
+        ADD to the runtime.
+*/
+
